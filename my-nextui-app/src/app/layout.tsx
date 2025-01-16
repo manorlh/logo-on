fe194b9037bbd12a57a34db252621339d980042d
manor lahagani <manor21@gmail.com>
2025-01-16 19:55:16 +0200

    fix seo

--- my-nextui-app/src/app/layout.tsx
+++ my-nextui-app/src/app/layout.tsx
@@ -8,7 +8,8 @@ import { Footer } from './components/Footer';
 import { Favicon } from './components/Favicon';
 import { LanguageWrapper } from './components/LanguageWrapper';
 import { metadata as siteMetadata } from './metadata';
-
+import { headers } from 'next/headers';
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = siteMetadata;
@@ -18,14 +19,20 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // Get the current URL path to determine language
+  const headersList = headers();
+  const pathname = headersList.get('x-pathname') || '';
+  const lang = pathname.split('/')[1] || 'en'; // Default to 'en' if no language found
+
   return (
-    <html lang="he" className="dark">
+    <html lang={lang} className="dark">
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body className={`${inter.className} dark:bg-gray-900 dark:text-gray-100 min-h-screen flex flex-col`}>
         <JsonLd />
+        <Analytics/>
         <Providers>
           <LanguageWrapper>
             <main className="flex-grow">
